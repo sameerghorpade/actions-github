982870ed8eaeebe6593d4812ed5d29e4707a9349
Sameer Ghorpade <sameerghorpade@hotmail.com>
2025-11-30 13:26:38 +0100

    eslint corrections

--- eslint.config.js
+++ eslint.config.js
@@ -1,22 +1,26 @@
-// eslint.config.js (for Flat Config)
 import js from "@eslint/js";
 import globals from "globals";
 import pluginReact from "eslint-plugin-react";
+import pluginJest from "eslint-plugin-jest";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
   {
-    files: ["**/*.{js,mjs,cjs,jsx}"],
-    plugins: { js, react: pluginReact },
-    extends: ["js/recommended", pluginReact.configs.flat.recommended],
-    languageOptions: { globals: globals.browser },
+    files: ["**/*.{js,jsx}"],
+    plugins: { js, react: pluginReact, jest: pluginJest },
+    extends: [
+      "js/recommended",
+      pluginReact.configs.flat.recommended,
+      "plugin:jest/recommended"
+    ],
+    languageOptions: {
+      globals: globals.browser
+    },
     rules: {
-      "react/react-in-jsx-scope": "off" // disable rule for React 17+
+      "react/react-in-jsx-scope": "off"
     },
     settings: {
-      react: {
-        version: "detect" // automatically detect React version
-      }
+      react: { version: "detect" }
     }
   }
 ]);
